24bc761fa53f4fec3a46b63b7f1f759a09a35539
Jonathan Cabrera <cjonthecrack773@gmail.com>
2024-09-05 18:46:16 -0500

    Parcial_1

--- Parciales/Parcial_1/Problema_1/analisis.php
+++ Parciales/Parcial_1/Problema_1/analisis.php
@@ -2,21 +2,21 @@
 include 'utilidades_texto.php';
 
 $frases = [
-    "La vida es bella",
-    "PHP es un lenguaje de programación",
-    "Aprender a programar es divertido"
+    "La vida es bonita",
+    "PHP es un lenguaje de programación bieen pritty loco ",
+    "Aprender a programar es divertido mas si lo haces para hackear"
 ];
 
-// Iniciar la tabla HTML
 echo "<table border='1' style='border-collapse: collapse; width: 70%;'>";
-echo "<tr><th>Frase Original</th><th>Número de Palabras</th><th>Número de Vocales</th><th>Palabras Invertidas</th></tr>";
+echo "<tr><th>Frase Original</th><th>Número de Palabras</th> <th>Número de Vocales</th><th>Palabras Invertidas</th></tr>";
 
 foreach ($frases as $frase) {
     $numPalabras = contar_palabras($frase);
     $numVocales = contar_vocales($frase);
     $fraseInvertida = invertir_palabras($frase);
 
-    // Mostrar los resultados en la tabla
+
+
     echo "<tr>";
     echo "<td>$frase</td>";
     echo "<td>$numPalabras</td>";
